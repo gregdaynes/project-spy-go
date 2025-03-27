25d109ec1f510817484d352033cf8684bccbf7b9
Gregory Daynes <gregdaynes@gmail.com>
2025-03-26 21:25:10 -0700

    refactor: move task related method to task.go

--- cmd/web/task.go
+++ cmd/web/task.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"time"
 )
 
@@ -30,4 +31,62 @@ func (t *Task) HasPriorityOrTags() bool {
 	}
 
 	return false
+}
+
+func getAvailableLanes(t *Task, lanes map[string]TaskLane) map[string]ViewLaneModel {
+	taskLanes := make(map[string]ViewLaneModel)
+
+	for name, lane := range lanes {
+		taskLanes[name] = ViewLaneModel{
+			Name:     lane.Name,
+			Slug:     lane.Slug,
+			Selected: t.Lane == lane.Name,
+		}
+	}
+
+	return taskLanes
+}
+
+func getAvailableActions(t *Task) map[string]ViewActionModel {
+	actions := make(map[string]ViewActionModel)
+
+	if t.Title == "" {
+		actions["save"] = ViewActionModel{
+			Label:  "Create",
+			Name:   "save",
+			Action: "/new/",
+			Method: http.MethodPost,
+		}
+
+		return actions
+	}
+
+	actions["view"] = ViewActionModel{
+		Label:  "View",
+		Name:   "view",
+		Method: http.MethodGet,
+		Action: "/view/" + t.Lane + "/" + t.Filename,
+	}
+
+	actions["save"] = ViewActionModel{
+		Label:  "Update",
+		Name:   "update",
+		Action: "/update/" + t.Lane + "/" + t.Filename,
+		Method: http.MethodPost,
+	}
+
+	actions["archive"] = ViewActionModel{
+		Label:  "Archive",
+		Name:   "archive",
+		Action: "/archive/" + t.Lane + "/" + t.Filename,
+		Method: http.MethodGet,
+	}
+	actions["delete"] = ViewActionModel{
+		Label:  "Delete",
+		Name:   "delete",
+		Action: "/delete/" + t.Lane + "/" + t.Filename,
+		Method: http.MethodGet,
+	}
+
+	return actions
 }
