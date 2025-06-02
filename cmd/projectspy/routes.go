d5501a5f5592e363e181d99bea9c404d567f6710
Gregory Daynes <gregdaynes@gmail.com>
2025-06-02 16:43:27 -0700

    fix: new task routing issue

--- cmd/projectspy/routes.go
+++ cmd/projectspy/routes.go
@@ -16,9 +16,10 @@ func (app *application) routes() http.Handler {
 		mux.Handle("GET /static/", http.FileServerFS(web.Files))
 	}
 	mux.Handle("GET /", http.HandlerFunc(app.home))
+	mux.Handle("GET /{tid}", http.HandlerFunc(app.viewById))
 	mux.Handle("GET /info", http.HandlerFunc(app.info))
-	mux.Handle("POST /new/", http.HandlerFunc(app.createTask))
-	mux.Handle("GET /new/", http.HandlerFunc(app.newTask))
+	mux.Handle("POST /new", http.HandlerFunc(app.createTask))
+	mux.Handle("GET /new", http.HandlerFunc(app.newTask))
 	mux.Handle("GET /view/{lane}/{filename}", http.HandlerFunc(app.view))
 	mux.Handle("POST /update/{lane}/{filename}", http.HandlerFunc(app.update))
 	mux.Handle("GET /delete/{lane}/{filename}", http.HandlerFunc(app.delete))
@@ -26,7 +27,6 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /archive/{lane}/{filename}", http.HandlerFunc(app.archive))
 	mux.Handle("POST /archive/{lane}/{filename}", http.HandlerFunc(app.archiveConfirm))
 	mux.Handle("GET /manifest.json", http.HandlerFunc(app.manifest))
-	mux.Handle("GET /{tid}", http.HandlerFunc(app.viewById))
 	mux.Handle("POST /attach-file", http.HandlerFunc(app.attachFile))
 
 	return mux
